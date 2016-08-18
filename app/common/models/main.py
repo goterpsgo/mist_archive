2a6845286f3bbf0a7ebf16b73d7ae79203a499af
Joe Tseng <jtseng@silab.net>
2016-08-18 10:57:28 -0400

    Added pool_recycle param to create_engine(), made it less than 8 hrs (MySQL default).

--- app/common/models/main.py
+++ app/common/models/main.py
@@ -21,4 +21,10 @@ connect_string = 'mysql://mistUser:m1$TD@t@B@$3!@#@mistDB:3306/MIST'
 ssl_args = {'ssl': {'cert': '/opt/mist_base/certificates/mist-interface.crt',
                     'key': '/opt/mist_base/certificates/mist-interface.key',
                     'ca': '/opt/mist_base/certificates/si_ca.crt'}}
-engine = create_engine(connect_string, connect_args=ssl_args, echo=False)
+
+# ========== NOTE ==========
+# MySQL times out after 8 hours of inactivity by default:
+# http://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_wait_timeout
+# added pool_recycle param (set to 6 hours) to create_engine to prevent connections from timing out
+# http://docs.sqlalchemy.org/en/latest/core/engines.html?highlight=pool_recycle#sqlalchemy.create_engine.params.pool_recycle
+engine = create_engine(connect_string, connect_args=ssl_args, echo=False, pool_recycle=21600)
