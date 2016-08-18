5a99d85fc0726b6e444970676d52c3dd0a67aced
Joe Tseng <jtseng@silab.net>
2016-08-18 11:02:10 -0400

    Added .currentUser for handling XSS. Removed extraneous code.

--- static/js/app_services/authentication.service.js
+++ static/js/app_services/authentication.service.js
@@ -1,3 +1,4 @@
+// code based on http://jasonwatmore.com/post/2016/04/05/AngularJS-JWT-Authentication-Example-Tutorial.aspx
 (function () {
     'use strict';
 
@@ -5,7 +6,7 @@
         .module('app')
         .factory('AuthenticationService', Service);
 
-    function Service($http, $localStorage) {
+    function Service($http, $localStorage, $sessionStorage) {
         var service = {};
 
         service.Login = Login;
@@ -19,64 +20,20 @@
             // var headers =
             // $http.post('/auth', data_as_json)
                 .success(function (response) {
-                    console.log('[success]');
                     // login successful if there's a token in the response
-                    if (response.data.access_token) {
-                        // store username and token in local storage to keep user logged in between page refreshes
-                        $localStorage.currentUser = { username: username, token: response.data.access_token };
-
-                        // add jwt token to auth header for all requests made by the $http service
-                        $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.access_token;
-
-                        // execute callback with true to indicate successful login
-                        callback(true);
-                    } else {
-                        // execute callback with false to indicate failed login
-                        callback(false);
-                    }
-                })
-                .error(function(data, status) {
-                    console.log('[ERROR] ', status, data);
-                });
-        }
-
-        function Logout() {
-            // remove user from local storage and clear http auth header
-            delete $localStorage.currentUser;
-            $http.defaults.headers.common.Authorization = '';
-        }
-    }
-})();
-(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .factory('AuthenticationService', Service);
-
-    function Service($http, $localStorage) {
-        var service = {};
-
-        service.Login = Login;
-        service.Logout = Logout;
-
-        return service;
-
-        function Login(username, password, callback) {
-            $http.post('/auth', { username: username, password: password })
-            // var data_as_json = angular.toJson({"username": username, "password": password});
-            // var headers =
-            // $http.post('/auth', data_as_json)
-                .success(function (response) {
-                    console.log('[success]');
-                    // login successful if there's a token in the response
+                    // code below based on http://www.redotheweb.com/2015/11/09/api-security.html
                     if (response.access_token) {
                         // store username and token in local storage to keep user logged in between page refreshes
+                        // for handling CSRF
                         $localStorage.currentUser = { username: username, token: response.access_token };
 
                         // add jwt token to auth header for all requests made by the $http service
                         $http.defaults.headers.common.Authorization = 'Bearer ' + response.access_token;
 
+                        // add token to session
+                        // for handling XSS
+                        $sessionStorage.currentUser = { username: username, token: response.access_token };
+
                         // execute callback with true to indicate successful login
                         callback(true);
                     } else {
@@ -93,6 +50,8 @@
         function Logout() {
             // remove user from local storage and clear http auth header
             delete $localStorage.currentUser;
+            // remove user from session
+            delete $sessionStorage.currentUser;
             $http.defaults.headers.common.Authorization = '';
         }
     }
