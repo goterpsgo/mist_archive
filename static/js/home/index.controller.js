9fcf51303f540f631b5f2d6a2ce1a9419ec70195
Joe Tseng <jtseng@silab.net>
2016-08-24 15:03:12 -0400

    [ng] Added get_users() for controller. Will need to remove test content at some point.

--- static/js/home/index.controller.js
+++ static/js/home/index.controller.js
@@ -5,12 +5,40 @@
         .module('app')
         .controller('Home.IndexController', Controller);
 
-    function Controller() {
+    function Controller($scope, MistUsersService) {
+        $scope.users;
+        $scope.status;
         var vm = this;
 
         initController();
 
         function initController() {
         }
+
+        function get_users() {
+            MistUsersService.get_users()
+                .then(
+                      function(users) {
+                        $scope.users = users;
+                        return users;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
+
+        function get_stuff() {
+            MistUsersService.get_stuff()
+                .then(
+                    function(stuff) {
+                        return stuff;
+                    }
+                );
+        }
+
+        $scope.add_user = function() {
+            console.log('Add user');
+        };
     }
 })();
