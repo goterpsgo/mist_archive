54c2e00a1b59a807f03792d14e9fffdffb80a5d3
Joe Tseng <jtseng@silab.net>
2016-08-25 11:58:45 -0400

    Added .currentUser.username to  for use in views.

--- static/js/mistusers/index.controller.js
+++ static/js/mistusers/index.controller.js
@@ -5,10 +5,12 @@
         .module('app')
         .controller('UserController', Controller);
 
-    function Controller($scope, MistUsersService) {
+    function Controller($scope, $sessionStorage, MistUsersService) {
         // $scope.my_stuff = this.get_stuff();
         // $scope.stuff = get_stuff();
         $scope.users = get_users();
+        $scope.username = $sessionStorage.currentUser.username;
+        // $scope.user = get_user_by_id(1);
 
         // function initController() {
         // };
@@ -26,6 +28,31 @@
 
         function get_users() {
             MistUsersService._get_users()
+                .then(
+                      function(users) {
+                          $scope.users = users;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
+
+        function get_user_by_id(id) {
+            MistUsersService._get_user_by_id(1)
+                .then(
+                      function(users) {
+                          $scope.user = user;
+                          return user;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
+
+        function get_user_by_username() {
+            MistUsersService._get_user_by_username()
                 .then(
                       function(users) {
                         $scope.users = users;
