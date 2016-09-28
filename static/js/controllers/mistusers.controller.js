c5fb8b38cc5fef11847c2deb6e4440ed1f3c319d
Joe Tseng <jtseng@silab.net>
2016-09-28 13:45:08 -0400

    Refactored nav menu build mechanism.

--- static/js/controllers/mistusers.controller.js
+++ static/js/controllers/mistusers.controller.js
@@ -30,6 +30,22 @@
                 .then(
                       function(users) {
                           $scope.users = users;
+                          $scope.menu_items = [];
+                          users.forEach(function(u) {
+                              // build $scope.menu_items collection to populate top nav menu
+                              if (u.username == $scope.username) {
+                                  $scope.menu_items.push({'text': 'Repos Auto Tags', 'state': 'repostag'});
+                                  $scope.menu_items.push({'text': 'Assets Manual Tags', 'state': 'assetstag'});
+                                  $scope.menu_items.push({'text': 'Publish', 'state': 'publish'});
+                                  if (u.permission == 'Admin User' || u.permission == 'Super User') {
+                                      $scope.menu_items.push({'text': 'Admin', 'state': 'admin'});
+                                      $scope.menu_items.push({'text': 'Config', 'state': 'config'});
+                                      $scope.menu_items.push({'text': 'Tag Activities', 'state': 'tagactivities'});
+                                      $scope.menu_items.push({'text': 'Local Logs', 'state': 'locallogs'});
+                                  }
+                                  $scope.menu_items.push({'text': 'Logout', 'state': 'login'});
+                              }
+                          });
                       }
                     , function(err) {
                         $scope.status = 'Error loading data: ' + err.message;
