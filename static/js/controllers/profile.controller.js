92e174040a1570f79901af07737458d28a3a31f0
Joe Tseng <jtseng@silab.net>
2016-11-30 16:47:29 -0500

    [copy and paste] Forgot one piece to update.

--- static/js/controllers/profile.controller.js
+++ static/js/controllers/profile.controller.js
@@ -3,7 +3,7 @@
  
     angular
         .module('app')
-        .controller('Publish.IndexController', Controller);
+        .controller('Profile.IndexController', Controller);
 
     function Controller() {
         var vm = this;
