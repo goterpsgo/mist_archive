acfb192548face149dc28252016c10316aa0a32e
Joe Tseng <jtseng@silab.net>
2016-12-06 10:04:20 -0500

    Updated endpoint URL NOTE: need to revert port back to 8443.

--- static/js/app_services/repos.service.js
+++ static/js/app_services/repos.service.js
@@ -17,7 +17,7 @@
 
         function get_repos() {
             var deferred = $q.defer();
-            $http.get('https://10.11.1.239:8443/api/v2/user/repos')
+            $http.get('https://10.11.1.239:8444/api/v2/repos')
                 .then(
                     function(response) {
                         deferred.resolve(response.data);
