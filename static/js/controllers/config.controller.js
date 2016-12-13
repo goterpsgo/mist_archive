5256835728236c821d413d7f5137548e36f6a10e
Joe Tseng <jtseng@silab.net>
2016-12-13 15:13:11 -0500

    Controller updates pane header value based on state.

--- static/js/controllers/config.controller.js
+++ static/js/controllers/config.controller.js
@@ -5,12 +5,25 @@
         .module('app')
         .controller('Config.IndexController', Controller);
 
-    function Controller() {
+    function Controller($scope, $state) {
         var vm = this;
+        $scope._task = '';
+        var obj_tasks = [];
+        obj_tasks['config.global_parameters'] = 'Global Parameters';
+        obj_tasks['config.set_banner_text'] = 'Set Banner Text';
+        obj_tasks['config.set_classification_level'] = 'Set Classification Level';
+        obj_tasks['config.manage_security_centers'] = 'Manage Security Centers';
+        obj_tasks['config.manage_publishing_sites'] = 'Manage Publishing Sites';
+        obj_tasks['config.remove_tag_definitions'] = 'Remove Tag Definitions';
 
         initController();
 
         function initController() {
+            $scope._task = obj_tasks[$state.current.name];
+        }
+        
+        $scope.select_task = function(_task) {
+            $scope._task = _task;
         }
     }
 })();
