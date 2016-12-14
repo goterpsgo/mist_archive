4da3815670f9f84370decb502b159e08e9c4716b
Joe Tseng <jtseng@silab.net>
2016-12-14 08:58:52 -0500

    Add load_sc_data() to populate .sc_list.

--- static/js/controllers/config.controller.js
+++ static/js/controllers/config.controller.js
@@ -5,7 +5,7 @@
         .module('app')
         .controller('Config.IndexController', Controller);
 
-    function Controller($scope, $state) {
+    function Controller($scope, $state, SecurityCentersService) {
         var vm = this;
         $scope._task = '';
         var obj_tasks = [];
@@ -15,15 +15,30 @@
         obj_tasks['config.manage_security_centers'] = 'Manage Security Centers';
         obj_tasks['config.manage_publishing_sites'] = 'Manage Publishing Sites';
         obj_tasks['config.remove_tag_definitions'] = 'Remove Tag Definitions';
+        $scope.sc_list = []
 
         initController();
 
         function initController() {
             $scope._task = obj_tasks[$state.current.name];
+            load_sc_data();
         }
         
         $scope.select_task = function(_task) {
             $scope._task = _task;
         }
+
+        function load_sc_data() {
+            SecurityCentersService
+                ._get_scs()
+                .then(
+                      function(security_centers) {
+                          $scope.sc_list = security_centers.sc_list;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        };
     }
 })();
