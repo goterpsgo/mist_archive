c2ab9902ad8e3c49b51fc7030eb721aee3f82a4a
Joe Tseng <jtseng@silab.net>
2016-12-21 12:16:52 -0500

    1. Added login() 2. Moved code originally in initController() into get_this_user().

--- static/js/controllers/nav.controller.js
+++ static/js/controllers/nav.controller.js
@@ -5,15 +5,22 @@
         .module('app')
         .controller('Nav.IndexController', Controller);
 
-    function Controller($scope, $localStorage, MistUsersService) {
+    function Controller($scope, $localStorage, MistUsersService, AuthenticationService, $location) {
         $scope.users;   // may not be needed...
         $scope.status;
         $scope.user;
+        $scope.selectedTab = '';
         var vm = this;
+        vm.login = login;
 
         initController();
 
         function initController() {
+            get_this_user();
+        }
+
+        function get_this_user() {
+            // populate $scope.user to provide user status and state to be used in the nav view.
             if (typeof $localStorage.currentUser !== 'undefined') {
                 // Cache results in $localStorage to minimize calls to database-driven RESTful endpoint
                 if (typeof $localStorage.user === 'undefined') {
@@ -34,6 +41,18 @@
                 // populate $scope.user to provide user status and state to be used in the nav view.
                 $scope.user = $localStorage.user;
             }
-        }
+        };
+
+        function login() {
+            vm.loading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (result) {
+                if (result === true) {
+                    $location.path('/');
+                } else {
+                    vm.error = 'Username or password is incorrect';
+                    vm.loading = false;
+                }
+            });
+        };
     }
 })();
