cdbc13e06e79b99634b542bd52039be29075fa08
Joe Tseng <jtseng@silab.net>
2016-12-21 15:33:10 -0500

    [profile.controller] Added get_this_user() and get_repos().

--- static/js/controllers/profile.controller.js
+++ static/js/controllers/profile.controller.js
@@ -5,12 +5,46 @@
         .module('app')
         .controller('Profile.IndexController', Controller);
 
-    function Controller() {
+    function Controller($scope, MistUsersService, ReposService, $localStorage, $timeout) {
         var vm = this;
+        $scope.profile = {};
+        $scope.repos = {};
 
         initController();
 
         function initController() {
+            get_this_user();
+
+            // Delay get_users() by 1ms to not overwhelm the database (unless there's a better solution - JWT 6 Dec 2016)
+            $timeout(function() {
+                get_repos();
+            }, 1);
         }
+
+        function get_this_user() {
+            var username = $localStorage.currentUser.username;
+            MistUsersService._get_user(username)
+                .then(
+                      function(user) {
+                          $scope.profile = user.users_list[0];
+                          console.log(user.users_list[0]);
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        };
+
+        function get_repos() {
+            ReposService._get_repos()
+                .then(
+                      function(repos) {
+                          $scope.repos = repos.repos_list;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        };
     }
 })();
