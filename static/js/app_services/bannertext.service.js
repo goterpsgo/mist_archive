ab526df7cbb8a5d32b77fb56ff147ef4c1595191
Joe Tseng <jtseng@silab.net>
2017-01-03 10:18:55 -0500

    Added banner text service.

---
+++ static/js/app_services/bannertext.service.js
@@ -0,0 +1,69 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('BannerTextService', Service);
+
+    function Service($http, $q, Upload) {
+        var factory = {
+              _get_bannertext: get_bannertext
+            , _insert_bannertext: insert_bannertext
+            , _delete_bannertext: delete_bannertext
+        };
+
+        return factory;
+
+        function get_bannertext() {
+            var deferred = $q.defer();
+            $http.get('https://10.11.1.239:8444/api/v2/bannertext')
+                .then(
+                    function(response) {
+                        deferred.resolve(response.data);
+                    }
+                );
+            return deferred.promise;
+        }
+
+        function insert_bannertext(form_data) {
+            var deferred = $q.defer();
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json;charset=utf-8;'
+                }
+            }
+
+            // since values from pulldown is in comma delimited list, list is converted in to obj
+            form_data.repos = serial_to_obj(form_data.repos);   // converting delimited strings into JSON objects
+
+            $http.post('https://10.11.1.239:8444/api/v2/user/bannertext', form_data, config)
+                .then(function(form_data, status, headers, config) {
+                    deferred.resolve(form_data);
+                }
+                , function(data, status, headers, config) {
+                    deferred.resolve(JSON.parse('{"response": {"method": "POST", "result": "error", "status": "' + status + '"}}'));
+                })
+            ;
+            return deferred.promise;
+        }
+
+        function delete_bannertext() {
+            var deferred = $q.defer();
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json;charset=utf-8;'
+                }
+            };
+
+            $http.delete('https://10.11.1.239:8444/api/v2/securitycenter/' + id)
+                .then(function(data, status, headers) {
+                    deferred.resolve(data);
+                }
+                , function(data, status, header, config) {
+                    deferred.resolve(JSON.parse('{"response": {"method": "POST", "result": "error", "status": "' + status + '"}}'));
+                }
+            );
+            return deferred.promise;
+        }
+    }
+})();
