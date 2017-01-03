08eb1d4d87e2fb304b7603ce2fd2f7b3b4d1227d
Joe Tseng <jtseng@silab.net>
2017-01-03 12:18:22 -0500

    Add load_banner_text() to load banner text to show in landing page.

--- static/js/controllers/login.controller.js
+++ static/js/controllers/login.controller.js
@@ -5,18 +5,33 @@
         .module('app')
         .controller('Login.IndexController', Controller);
 
-    function Controller($location, AuthenticationService) {
+    function Controller($scope, $location, AuthenticationService, BannerTextService) {
         var vm = this;
-            console.log('Got here');
 
         vm.login = login;
+        $scope.banner_text = {};
 
         initController();
 
         function initController() {
             // reset login status
             AuthenticationService.Logout();
+            load_banner_text();
         };
+
+        function load_banner_text() {
+            BannerTextService
+                ._get_bannertext()
+                .then(
+                      function(banner_text) {
+                          $scope.banner_text['banner_text'] = banner_text.banner_text;
+
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
 
         function login() {
             vm.loading = true;
