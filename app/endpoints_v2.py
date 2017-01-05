64c69be87f1a068e971d9145aa3075ce7019a10f
Joe Tseng <jtseng@silab.net>
2017-01-05 14:22:51 -0500

    [endpoints_v2] Updated MistParams.put().

--- app/endpoints_v2.py
+++ app/endpoints_v2.py
@@ -45,6 +45,9 @@ def rs_banner_text():
 def rs_classification():
     return main.session.query(main.Classifications)
 
+def rs_mist_params():
+    return main.session.query(main.MistParams)
+
 # http://stackoverflow.com/a/1960546/6554056
 def row_to_dict(row):
     d = {}
@@ -941,7 +944,6 @@ class Classification(Resource):
             # update all "selected" values as N
             upd_form = {}
             upd_form["selected"] = "N"
-            # pdb.set_trace()
 
             classification_selected = rs_classification().filter(main.Classifications.selected == 'Y')
             classification_selected.update(upd_form)
@@ -968,26 +970,16 @@ class Classification(Resource):
 
 
 class MistParams(Resource):
-    # @jwt_required()
+    @jwt_required()
     def get(self):
         rs_dict = {}  # used to hold and eventually return users_list[] recordset and associated metadata
+        rs_dict['Authorization'] = create_new_token(request)  # pass token via response data since I can't figure out how to pass it via response header - JWT Oct 2016
 
-        rs_classification_handle = rs_classification().order_by(main.Classifications.index)
-        r_single_classification = None
-        if _id is not None:
-            r_single_classification = rs_classification_handle.filter(
-                main.Classifications.selected == "Y"
-            ).first()
-
-        # add results to classifications_list
-        classifications_list = []
-        if _id is None:
-            for r_classification in rs_classification_handle:
-                classifications_list.append(row_to_dict(r_classification))
-        else:
-            classifications_list.append(row_to_dict(r_single_classification))
-
-        rs_dict['classifications_list'] = classifications_list  # add users_list[] to rs_dict
+        # add results to mist_params_list[]
+        mist_params_list = []
+        for r_mist_param in rs_mist_params():
+            mist_params_list.append(row_to_dict(r_mist_param))
+        rs_dict['mist_params_list'] = mist_params_list  # add mist_params_list[] to rs_dict
 
         return jsonify(rs_dict)  # return rs_dict
 
@@ -997,25 +989,15 @@ class MistParams(Resource):
     # @jwt_required()
     def put(self, _field_name, _value):
         try:
-            # update all "selected" values as N
             upd_form = {}
-            upd_form["selected"] = "N"
-            # pdb.set_trace()
+            upd_form[_field_name] = _value
 
-            classification_selected = rs_classification().filter(main.Classifications.selected == 'Y')
-            classification_selected.update(upd_form)
+            rs_mist_params().update(upd_form)
 
             main.session.commit()
             main.session.flush()
 
-            # update "selected" as Y where index = _id
-            upd_form["selected"] = "Y"
-            classification_selected = rs_classification().filter(main.Classifications.index == _id)
-            classification_selected.update(upd_form)
-            main.session.commit()
-            main.session.flush()
-
-            return {'response': {'method': 'PUT', 'result': 'success', 'message': 'Classification successfully updated.', 'class': 'alert alert-success', '_id': int(_id)}}
+            return {'response': {'method': 'PUT', 'result': 'success', 'message': 'Parameter successfully updated.', 'class': 'alert alert-success', '_value': int(_value)}}
 
         except (main.ProgrammingError) as e:
             print ("[ProgrammingError] PUT /api/v2/securitycenter/%s / %s" % (_id,e))
@@ -1034,4 +1016,4 @@ api.add_resource(Repos, '/repos')
 api.add_resource(SecurityCenter, '/securitycenters', '/securitycenter/<int:_id>')
 api.add_resource(BannerText, '/bannertext')
 api.add_resource(Classification, '/classifications', '/classification/<string:_id>')
-api.add_resource(MistParams, '/params', '/params/<string:_field_name>/<int:_value>')
+api.add_resource(MistParams, '/params', '/param/<string:_field_name>/<int:_value>')
