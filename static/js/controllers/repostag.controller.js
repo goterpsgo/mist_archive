d8fa83da71578cee2d2d4fb65bdfe6fb05689c17
Joe Tseng <jtseng@silab.net>
2017-01-31 16:00:54 -0500

    1. Select single/multi based on category cardinality 2. Added get_this_user() to return user assigned repos.

--- static/js/controllers/repostag.controller.js
+++ static/js/controllers/repostag.controller.js
@@ -5,13 +5,15 @@
         .module('app')
         .controller('Repostag.IndexController', Controller);
 
-    function Controller($scope, TagDefinitionsService, CategorizedTagsService) {
+    function Controller($scope, $localStorage, MistUsersService, TagDefinitionsService, CategorizedTagsService) {
         var vm = this;
         $scope.tag_definitions = {};
         $scope.assigned_tag_definition = {"value": 23};
         $scope.categorized_tags = {};
         $scope.treeData = {"value": "Loading..."};
         $scope.tags_tree = null;
+        $scope.profile = {};
+        $scope.cardinality = {};
 
         initController();
 
@@ -22,6 +24,7 @@
             // $scope.tags_tree = $$("tags_tree");
             console.log('[23] initController()');
             // console.log($scope.tags_tree);
+            get_this_user();
         }
 
         function load_tag_definitions() {
@@ -31,6 +34,11 @@
                 .then(
                     function(results) {
                         $scope.tag_definitions = results.tag_definitions;
+                        for (var _cnt = 0; _cnt < $scope.tag_definitions.length; _cnt++) {
+                            $scope.cardinality[$scope.tag_definitions[_cnt]['id']] = $scope.tag_definitions[_cnt]['cardinality'];
+                        }
+                        console.log('[36] $scope.cardinality: ');
+                        console.log($scope.cardinality);
                         vm.loading = false;
                       }
                     , function(err) {
@@ -48,6 +56,8 @@
                 .then(
                       function(results) {
                         $scope.treeData = results.treeData;
+                        // single or multiple tree node select based on cardinality
+                        $$("tags_tree").config.select = ($scope.cardinality[_id] == 1) ? 'select' : 'multiselect';
                         vm.loading = false;
                       }
                     , function(err) {
@@ -56,7 +66,41 @@
                       }
                 )
             ;
-        };
+        }
+
+        function get_repos() {
+            ReposService._get_repos()
+                .then(
+                      function(repos) {
+                          $scope.assign_repos = repos.repos_list;
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
+
+        function get_this_user() {
+            var username = $localStorage.currentUser.username;
+            MistUsersService._get_user(username)
+                .then(
+                      function(user) {
+                          $scope.profile = user.users_list[0];
+
+                          // if there are assigned repos for given user, add .repos[] entries to bound .assign_repos array for dropdown selections
+                          if (Object.keys($scope.profile.repos).length > 0) {
+                              $scope.profile.assign_repos = [];
+                              angular.forEach($scope.profile.repos, function(repo, key) {
+                                  var selected_repo_entry = {'repo_id': repo.repoID, 'sc_id': repo.scID};
+                                  $scope.profile.assign_repos.push(selected_repo_entry);
+                              })
+                          }
+                      }
+                    , function(err) {
+                        $scope.status = 'Error loading data: ' + err.message;
+                      }
+                );
+        }
 
         $scope.load_tags = function() {
             load_categorized_tags($scope.assigned_tag_definition.id);
@@ -68,8 +112,8 @@
 
         $scope.do_this = function(id, details) {
             console.log('[65] do_this(): ');
-            // console.log($$("tags_tree").getSelectedId());
-            console.log($$("tags_tree").getChecked());
+            console.log($$("tags_tree").getSelectedId());
+            // console.log($$("tags_tree").getChecked());
         }
     }
 })();
