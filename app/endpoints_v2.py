4015045e6fde24e7338231cb99df23217276dccc
Joe Tseng <jtseng@silab.net>
2017-02-01 12:53:04 -0500

    [class Users] Added tags affiliated with repos.

--- app/endpoints_v2.py
+++ app/endpoints_v2.py
@@ -58,6 +58,9 @@ def rs_publish_sites():
 def rs_tags():
     return main.session.query(main.Tags)
 
+def rs_tagged_repos():
+    return main.session.query(main.TaggedRepos)
+
 # http://stackoverflow.com/a/1960546/6554056
 def row_to_dict(row):
     d = {}
@@ -221,10 +224,35 @@ def create_user_dict(obj_user):
                     , 'class': 'checkbox-inline'
                     , 'class_glyph': 'checkbox-inline glyphicon glyphicon-ok-sign text-success'
                     , 'title': 'Assigned repo; click to unassign.'
+                    , 'tags': []
                 }
                 # Add repo if key doesn't yet exist in users['repos'] dict
                 if ("identifier" not in user['repos']):
                     user['repos'][identifier] = repo
+
+                obj_tagged_repos = rs_tagged_repos().filter(
+                    main.and_(
+                          main.TaggedRepos.scID == obj_repo_access.scID
+                        , main.TaggedRepos.repoID == obj_repo_access.repoID
+                        , main.TaggedRepos.status == "True"
+                    )
+                )
+
+                if int(obj_tagged_repos.count()) != 0:
+                    for obj_tagged_repo in obj_tagged_repos:
+                        obj_tags = rs_tags().filter(
+                              main.and_(
+                                  main.Tags.nameID == obj_tagged_repo.tagID
+                                , main.Tags.rollup == obj_tagged_repo.rollup
+                              )
+                        ).order_by(main.Tags.dname)
+                        if int(obj_tags.count()) != 0:
+                            for obj_tag in obj_tags:
+                                tag = {
+                                    'dname': obj_tag.dname
+                                }
+                                user['repos'][identifier]['tags'].append(tag)
+
             user['cnt_repos'] += 1    # if a user has at least one repo assigned then value > 0
     return user
 
@@ -1211,62 +1239,49 @@ class CategorizedTags(Resource):
 
         return jsonify(rs_dict)  # return rs_dict
 
-    @jwt_required()
+    # @jwt_required()
     def post(self):
         try:
             form_fields = request.get_json(force=True)
 
-            new_entry = main.SomeModel(
-                  name = form_fields['name']
-                , description = form_fields['description'] if ("description" in form_fields) else "TBD"
-                , defaultValue = form_fields['defaultValue'] if ("defaultValue" in form_fields) else None
-                , type = form_fields['type'] if ("type" in form_fields) else "plaintext"
-                , cardinality = form_fields['cardinality'] if ("cardinality" in form_fields) else 1
-                , timestamp = datetime.now()
-            )
+            print "[1219] form_fields: %r" % form_fields
 
-            main.session.add(new_entry)
-            main.session.commit()
-            main.session.flush()
-
-            return {'response': {'method': 'POST', 'result': 'success', 'message': 'New something entry submitted.', 'class': 'alert alert-success', 'id': int(new_entry.id)}}
+        #     new_entry = main.SomeModel(
+        #           name = form_fields['name']
+        #         , description = form_fields['description'] if ("description" in form_fields) else "TBD"
+        #         , defaultValue = form_fields['defaultValue'] if ("defaultValue" in form_fields) else None
+        #         , type = form_fields['type'] if ("type" in form_fields) else "plaintext"
+        #         , cardinality = form_fields['cardinality'] if ("cardinality" in form_fields) else 1
+        #         , timestamp = datetime.now()
+        #     )
+        #
+        #     main.session.add(new_entry)
+        #     main.session.commit()
+        #     main.session.flush()
+        #
+            # return {'response': {'method': 'POST', 'result': 'success', 'message': 'New something entry submitted.', 'class': 'alert alert-success', 'id': int(new_entry.id)}}
+            return {'response': {'method': 'POST', 'result': 'success', 'message': 'New something entry submitted.', 'class': 'alert alert-success', 'id': 0}}
 
         except (TypeError) as e:
             print ("[TypeError] POST /api/v2/someclass / %s" % e)
             main.session.rollback()
             return {'response': {'method': 'POST', 'result': 'error', 'message': str(e), 'class': 'alert alert-danger'}}
-        except (ValueError) as e:
-            print ("[ValueError] POST /api/v2/someclass / %s" % e)
-            main.session.rollback()
-            return {'response': {'method': 'POST', 'result': 'error', 'message': str(e), 'class': 'alert alert-danger'}}
-        except (main.IntegrityError) as e:
-            print ("[IntegrityError] POST /api/v2/someclass / %s" % e)
-            main.session.rollback()
-            return {'response': {'method': 'POST', 'result': 'error', 'message': 'Submitted something already exists.', 'class': 'alert alert-danger'}}
+        # except (ValueError) as e:
+        #     print ("[ValueError] POST /api/v2/someclass / %s" % e)
+        #     main.session.rollback()
+        #     return {'response': {'method': 'POST', 'result': 'error', 'message': str(e), 'class': 'alert alert-danger'}}
+        # except (main.IntegrityError) as e:
+        #     print ("[IntegrityError] POST /api/v2/someclass / %s" % e)
+        #     main.session.rollback()
+        #     return {'response': {'method': 'POST', 'result': 'error', 'message': 'Submitted something already exists.', 'class': 'alert alert-danger'}}
 
-    @jwt_required()
+    # @jwt_required()
     def put(self, _id):
-        try:
-            upd_form = request.get_json(force=True)
+        return {'response': {'method': 'PUT', 'result': 'success', 'message': 'No POST method for this endpoint.', 'class': 'alert alert-warning'}}
 
-            rs_some_model().filter(main.SomeModel.id == _id).update(upd_form)
-
-            main.session.commit()
-            main.session.flush()
-
-            return {'response': {'method': 'PUT', 'result': 'success', 'message': 'Some value successfully updated.', 'class': 'alert alert-success', '_id': _id}}
-
-        except (main.ProgrammingError) as e:
-            print ("[ProgrammingError] PUT /api/v2/someclass/%s / %s" % (_id,e))
-            main.session.rollback()
-            return {'response': {'method': 'PUT', 'result': 'ProgrammingError', 'message': e, 'class': 'alert alert-danger'}}
-
-    @jwt_required()
+    # @jwt_required()
     def delete(self, _id):
-        main.session.query(main.SomeModel).filter(main.SomeModel.id == _id).delete()
-        main.session.commit()
-        main.session.flush()
-        return {'response': {'method': 'DELETE', 'result': 'success', 'message': 'Some item successfully deleted.', 'class': 'alert alert-success', 'id': int(_id)}}
+        return {'response': {'method': 'DELETE', 'result': 'success', 'message': 'No POST method for this endpoint.', 'class': 'alert alert-warning'}}
 
 
 # # Generic model class template
@@ -1353,4 +1368,4 @@ api.add_resource(Classification, '/classifications', '/classification/<string:_i
 api.add_resource(MistParams, '/params', '/param/<string:_field_name>/<int:_value>')
 api.add_resource(TagDefinitions, '/tagdefinitions', '/tagdefinition/<int:_id>')
 api.add_resource(PublishSites, '/publishsites', '/publishsite/<int:_id>')
-api.add_resource(CategorizedTags, '/categorizedtags/<int:_td_id>')
+api.add_resource(CategorizedTags, '/categorizedtags', '/categorizedtags/<int:_td_id>')
