92ef4f5f13871096212a16ae348a63fe97773efb
Joe Tseng <jtseng@silab.net>
2017-02-02 16:22:10 -0500

    Added insert_categorizedtags().

--- static/js/app_services/categorizedtags.service.js
+++ static/js/app_services/categorizedtags.service.js
@@ -8,6 +8,7 @@
     function Service($http, $q, __env) {
         var factory = {
               _get_categorizedtags: get_categorizedtags
+            , _insert_categorizedtags: insert_categorizedtags
         };
 
         return factory;
@@ -24,6 +25,26 @@
                         deferred.resolve(JSON.parse('{"response": {"method": "GET", "result": "error", "status": "' + status + '"}}'));
                     }
                 );
+            return deferred.promise;
+        }
+
+
+        function insert_categorizedtags(form_data) {
+            var deferred = $q.defer();
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json;charset=utf-8;'
+                }
+            }
+
+            $http.post(__env.api_url + ':' + __env.port + '/api/v2/categorizedtags', form_data, config)
+                .then(function(form_data, status, headers, config) {
+                    deferred.resolve(form_data);
+                }
+                , function(data, status, headers, config) {
+                    // deferred.resolve(response.data.response);
+                    deferred.resolve(JSON.parse('{"response": {"method": "POST", "result": "error", "status": "' + status + '"}}'));
+                });
             return deferred.promise;
         }
     }
