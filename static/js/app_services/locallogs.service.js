e48fae9217a6ff458553c36c053513f3745f66f4
Joe Tseng <jtseng@silab.net>
2017-03-09 17:09:44 -0500

    Locallog service methods.

--- static/js/app_services/locallogs.service.js
+++ static/js/app_services/locallogs.service.js
@@ -3,20 +3,20 @@
 
     angular
         .module('app')
-        .factory('BannerTextService', Service);
+        .factory('LocalLogsService', Service);
 
     function Service($http, $q, __env) {
         var factory = {
-              _get_bannertext: get_bannertext
-            , _insert_bannertext: insert_bannertext
-            , _delete_bannertext: delete_bannertext
+              _get_local_log: get_local_log
+            , _get_local_logs: get_local_logs
         };
 
         return factory;
 
-        function get_bannertext() {
+        function get_local_logs() {
+            console.log('[locallogs.service:get_local_logs():17] Got here');
             var deferred = $q.defer();
-            $http.get(__env.api_url + ':' + __env.port + '/api/v2/bannertext')
+            $http.get(__env.api_url + ':' + __env.port + '/api/v2/locallogs')
                 .then(
                     function(response) {
                         deferred.resolve(response.data);
@@ -28,41 +28,17 @@
             return deferred.promise;
         }
 
-        function insert_bannertext(form_data) {
+        function get_local_log(_name) {
             var deferred = $q.defer();
-            var config = {
-                headers : {
-                    'Content-Type': 'application/json;charset=utf-8;'
-                }
-            }
-
-            $http.post(__env.api_url + ':' + __env.port + '/api/v2/bannertext', form_data, config)
-                .then(function(form_data, status, headers, config) {
-                    deferred.resolve(form_data);
-                }
-                , function(data, status, headers, config) {
-                    deferred.resolve(JSON.parse('{"response": {"method": "POST", "result": "error", "status": "' + status + '"}}'));
-                })
-            ;
-            return deferred.promise;
-        }
-
-        function delete_bannertext() {
-            var deferred = $q.defer();
-            var config = {
-                headers : {
-                    'Content-Type': 'application/json;charset=utf-8;'
-                }
-            };
-
-            $http.delete(__env.api_url + ':' + __env.port + '/api/v2/bannertext')
-                .then(function(data, status, headers) {
-                    deferred.resolve(data);
-                }
-                , function(data, status, header, config) {
-                    deferred.resolve(JSON.parse('{"response": {"method": "DELETE", "result": "error", "status": "' + status + '"}}'));
-                }
-            );
+            $http.get(__env.api_url + ':' + __env.port + '/api/v2/locallog/' + _name)
+                .then(
+                    function(response) {
+                        deferred.resolve(response.data);
+                    }
+                    , function(data, status, headers, config) {
+                        deferred.resolve(JSON.parse('{"response": {"method": "GET", "result": "error", "status": "' + status + '"}}'));
+                    }
+                );
             return deferred.promise;
         }
     }
